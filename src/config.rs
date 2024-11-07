20d24c703de7a450554fa0cc10d622632accd81d
Mark Story <mark@mark-story.com>
2024-11-07 15:58:50 -0500

    Add configuration options from wiki

--- src/config.rs
+++ src/config.rs
@@ -37,8 +37,25 @@ pub struct Config {
     /// The topic to fetch task messages from.
     pub kafka_topic: String,
 
+    /// The kafka topic to publish dead letter messages on
+    pub kafka_deadletter_topic: String,
+
     /// The path to the sqlite database
     pub db_path: String,
+
+    /// The maximum number of pending records that can be
+    /// in the InflightTaskStore
+    pub max_pending_count: u32,
+
+    /// The maximum value for processing deadlines. If a task
+    /// does not have a processing deadline set, this will be used.
+    pub max_processing_deadline: u32,
+
+    /// The maximum duration a task can be in InflightTaskStore
+    /// After this time, messages will be deadlettered if they 
+    /// are not complete. This should be a multiple of max_processing_deadline
+    /// to allow temporary worker deaths to be resolved.
+    pub deadletter_deadline: u32,
 }
 
 impl Default for Config {
@@ -50,7 +67,11 @@ impl Default for Config {
             statsd_addr: "127.0.0.1:8126".parse().unwrap(),
             kafka_cluster: vec!["127.0.0.1:9092".to_owned()],
             kafka_topic: "task-worker".to_owned(),
+            kafka_deadletter_topic: "task-worker-dlq".to_owned(),
             db_path: "./taskworker-inflight.sqlite".to_owned(),
+            max_pending_count: 200,
+            max_processing_deadline: 300,
+            deadletter_deadline: 900,
         }
     }
 }
@@ -80,6 +101,7 @@ mod tests {
         assert_eq!(config.log_level, Some(LogLevel::Info));
         assert_eq!(config.kafka_topic, "task-worker");
         assert_eq!(config.db_path, "./taskworker-inflight.sqlite");
+        assert_eq!(config.max_pending_count, 200);
     }
 
     #[test]
@@ -94,7 +116,11 @@ mod tests {
                 statsd_addr: 127.0.0.1:8126
                 kafka_cluster: [10.0.0.1:9092, 10.0.0.2:9092]
                 kafka_topic: error-tasks
+                kafka_deadletter_topic: error-tasks-dlq
                 db_path: ./taskworker-error.sqlite
+                max_pending_count: 512
+                max_processing_deadline: 1000
+                deadletter_deadline: 2000
             "#,
             )?;
             jail.set_env("TASKWORKER_LOG_LEVEL", "error");
@@ -103,7 +129,11 @@ mod tests {
             assert_eq!(config.sentry_dsn, Some("fake_dsn".to_owned()));
             assert_eq!(config.log_level, Some(LogLevel::Error));
             assert_eq!(config.kafka_topic, "error-tasks".to_owned());
+            assert_eq!(config.kafka_deadletter_topic, "error-tasks-dlq".to_owned());
             assert_eq!(config.db_path, "./taskworker-error.sqlite".to_owned());
+            assert_eq!(config.max_pending_count, 512);
+            assert_eq!(config.max_processing_deadline, 1000);
+            assert_eq!(config.deadletter_deadline, 2000);
 
             Ok(())
         });
