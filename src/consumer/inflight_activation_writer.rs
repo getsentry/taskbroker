9019744c93f239e079c73e24bfadac077bbec120
John <john.yang@sentry.io>
2024-11-13 14:09:48 -0500

    feat(kafka): Implement basic async kafka consumer (#17)
    Co-authored-by: Evan Hicks <evanh@users.noreply.github.com>

---
+++ src/consumer/inflight_activation_writer.rs
@@ -0,0 +1,84 @@
+use std::{mem::replace, sync::Arc, time::Duration};
+
+use tracing::info;
+
+use crate::inflight_activation_store::{InflightActivation, InflightActivationStore};
+
+use super::kafka::{
+    ReduceConfig, ReduceShutdownBehaviour, ReduceShutdownCondition, Reducer,
+    ReducerWhenFullBehaviour,
+};
+
+pub struct Config {
+    pub max_buf_len: usize,
+    pub max_pending_activations: usize,
+    pub flush_interval: Option<Duration>,
+    pub when_full_behaviour: ReducerWhenFullBehaviour,
+    pub shutdown_behaviour: ReduceShutdownBehaviour,
+}
+
+pub struct InflightActivationWriter {
+    store: Arc<InflightActivationStore>,
+    buffer: Vec<InflightActivation>,
+    config: Config,
+}
+
+impl InflightActivationWriter {
+    pub fn new(store: Arc<InflightActivationStore>, config: Config) -> Self {
+        Self {
+            store,
+            buffer: Vec::with_capacity(config.max_buf_len),
+            config,
+        }
+    }
+}
+
+impl Reducer for InflightActivationWriter {
+    type Input = InflightActivation;
+
+    type Output = ();
+
+    async fn reduce(&mut self, t: Self::Input) -> Result<(), anyhow::Error> {
+        self.buffer.push(t);
+        Ok(())
+    }
+
+    async fn flush(&mut self) -> Result<Self::Output, anyhow::Error> {
+        if self.buffer.is_empty() {
+            return Ok(());
+        }
+        let res = self
+            .store
+            .store(replace(
+                &mut self.buffer,
+                Vec::with_capacity(self.config.max_buf_len),
+            ))
+            .await?;
+        info!("Inserted {:?} entries", res.rows_affected);
+        Ok(())
+    }
+
+    fn reset(&mut self) {
+        self.buffer.clear();
+    }
+
+    async fn is_full(&self) -> bool {
+        self.buffer.len() >= self.config.max_buf_len
+            || self
+                .store
+                .count_pending_activations()
+                .await
+                .expect("Error communicating with activation store")
+                + self.buffer.len()
+                >= self.config.max_pending_activations
+    }
+
+    fn get_reduce_config(&self) -> ReduceConfig {
+        ReduceConfig {
+            shutdown_condition: ReduceShutdownCondition::Signal,
+            shutdown_behaviour: ReduceShutdownBehaviour::Flush,
+            when_full_behaviour: self.config.when_full_behaviour,
+            flush_interval: self.config.flush_interval,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use anyhow::Error;
 use clap::Parser;
 use config::Config;
-use inflight_task_store::InflightTaskStore;
+use consumer::{
+    deserialize_activation::{self},
+    inflight_activation_writer::{self, InflightActivationWriter},
+    kafka::{start_consumer, ReduceShutdownBehaviour, ReducerWhenFullBehaviour},
+    os_stream_writer::{OsStream, OsStreamWriter},
+};
+use inflight_activation_store::InflightActivationStore;
+use rdkafka::{config::RDKafkaLogLevel, ClientConfig};
+use std::{sync::Arc, time::Duration};
+use tokio::{select, signal, time};
+use tracing::info;
 
+#[allow(dead_code)]
 mod config;
 #[allow(dead_code)]
-mod inflight_task_store;
+mod consumer;
+#[allow(dead_code)]
+mod inflight_activation_store;
 mod logging;
 mod metrics;
 
@@ -23,13 +36,65 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // Read command line options
     let args = Args::parse();
     let config = Config::from_args(&args)?;
 
     logging::init(logging::LoggingConfig::from_config(&config));
     metrics::init(metrics::MetricsConfig::from_config(&config));
 
-    InflightTaskStore::new(&config.db_path).await?;
-    Ok(())
+    let store = Arc::new(InflightActivationStore::new(&config.db_path).await?);
+    let rpc_store = store.clone();
+
+    tokio::spawn(async move {
+        let mut timer = time::interval(Duration::from_millis(200));
+        loop {
+            select! {
+                _ = signal::ctrl_c() => {
+                    break;
+                }
+                _ = timer.tick() => {
+                    let _ = rpc_store.get_pending_activation().await;
+                    info!(
+                        "Pending activation in store: {}",
+                        rpc_store.count_pending_activations().await.unwrap()
+                    );
+                }
+            }
+        }
+    });
+
+    start_consumer(
+        [&config.kafka_topic as &str].as_ref(),
+        ClientConfig::new()
+            .set("group.id", "test-taskworker-consumer")
+            .set("bootstrap.servers", "127.0.0.1:9092")
+            .set("enable.partition.eof", "false")
+            .set("session.timeout.ms", "6000")
+            .set("enable.auto.commit", "true")
+            .set("auto.commit.interval.ms", "5000")
+            .set("enable.auto.offset.store", "false")
+            .set_log_level(RDKafkaLogLevel::Debug),
+        processing_strategy!({
+            map: deserialize_activation::new(deserialize_activation::Config {
+                deadletter_duration: None,
+            }),
+
+            reduce: InflightActivationWriter::new(
+                store.clone(),
+                inflight_activation_writer::Config {
+                    max_buf_len: 128,
+                    max_pending_activations: 2048,
+                    flush_interval: None,
+                    when_full_behaviour: ReducerWhenFullBehaviour::Flush,
+                    shutdown_behaviour: ReduceShutdownBehaviour::Drop,
+                }
+            ),
+
+            err: OsStreamWriter::new(
+                Duration::from_secs(1),
+                OsStream::StdErr,
+            ),
+        }),
+    )
+    .await
 }
