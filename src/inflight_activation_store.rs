ec2cda51a600f0a11b2ed5a0bc44acb2fe628cb4
Evan Hicks <evanh@users.noreply.github.com>
2024-11-15 16:42:52 -0500

    feat: Add gRPC server to handle requests from workers (#21)
    Add a gRPC server to the main executable.
    
    This server can handle two operations:
    
    - get_task: Will fetch a task with a status of pending, update the status to processing, and return it in the response.
    - set_task_status: Given an id and an i32 representing the status, update the task in the DB. Also lookup another task (in the same way as the function above) and return that if fetch_next is sent in the request.
    
    Other changes:
    
    - Put the consumer, upkeep and grpc server into separate tokio::spawn and await all 3 so they can run in parallel
    - Add an integration test creates a new InflightActivationStore and call the gRPC server to interact with it.
    - Moves the module calls into lib.rs, to allow the integration test to reference the modules.

--- src/inflight_activation_store.rs
+++ src/inflight_activation_store.rs
@@ -235,6 +235,13 @@ impl InflightActivationStore {
         )
     }
 
+    pub async fn clear(&self) -> Result<(), Error> {
+        sqlx::query("DELETE FROM inflight_taskactivations")
+            .execute(&self.sqlite_pool)
+            .await?;
+        Ok(())
+    }
+
     /// Update tasks that are in processing and have exceeded their processing deadline
     pub async fn handle_processing_deadline(&self) -> Result<u64, Error> {
         // Find rows past processing deadlines
@@ -732,5 +739,59 @@ mod tests {
             .await
             .expect("no error")
             .is_some());
+    }
+
+    #[tokio::test]
+    async fn test_clear() {
+        let url = generate_temp_filename();
+        let store = InflightActivationStore::new(&url).await.unwrap();
+
+        #[allow(deprecated)]
+        let batch = vec![InflightActivation {
+            activation: TaskActivation {
+                id: "id_0".into(),
+                namespace: "namespace".into(),
+                taskname: "taskname".into(),
+                parameters: "{}".into(),
+                headers: HashMap::new(),
+                received_at: Some(prost_types::Timestamp {
+                    seconds: 0,
+                    nanos: 0,
+                }),
+                deadline: None,
+                retry_state: None,
+                processing_deadline_duration: 0,
+                expires: Some(1),
+            },
+            status: TaskActivationStatus::Pending,
+            partition: 0,
+            offset: 0,
+            added_at: Utc::now(),
+            deadletter_at: None,
+            processing_deadline: None,
+        }];
+        assert!(store.store(batch).await.is_ok());
+
+        let result = sqlx::query(
+            "SELECT count() as count
+             FROM inflight_taskactivations;",
+        )
+        .fetch_one(&SqlitePool::connect(&url).await.unwrap())
+        .await
+        .unwrap();
+
+        assert_eq!(result.get::<u32, &str>("count"), 1);
+
+        assert!(store.clear().await.is_ok());
+
+        let result = sqlx::query(
+            "SELECT count() as count
+             FROM inflight_taskactivations;",
+        )
+        .fetch_one(&SqlitePool::connect(&url).await.unwrap())
+        .await
+        .unwrap();
+
+        assert_eq!(result.get::<u32, &str>("count"), 0);
     }
 }
