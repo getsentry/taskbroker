67e6420f81b4467afa8a58243eaf8f1df86e3ba9
Mark Story <mark@mark-story.com>
2024-11-26 14:15:54 -0500

    Add tests for kafka publishing

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,8 +1,19 @@
+use futures::StreamExt;
+use prost::Message as ProstMessage;
 use rand::Rng;
-use std::collections::HashMap;
+use rdkafka::{
+    admin::{AdminClient, AdminOptions, NewTopic, TopicReplication},
+    consumer::{Consumer, StreamConsumer},
+    producer::FutureProducer,
+    Message,
+};
+use std::{collections::HashMap, sync::Arc};
 
-use crate::inflight_activation_store::{
-    InflightActivation, InflightActivationStore, TaskActivationStatus,
+use crate::{
+    config::Config,
+    inflight_activation_store::{
+        InflightActivation, InflightActivationStore, TaskActivationStatus,
+    },
 };
 use chrono::Utc;
 use sentry_protos::sentry::v1::TaskActivation;
@@ -46,11 +57,111 @@ pub fn make_activations(count: u32) -> Vec<InflightActivation> {
     records
 }
 
+/// Assert that the inflight store has a given number of records with a status.
 pub async fn assert_count_by_status(
     store: &InflightActivationStore,
     status: TaskActivationStatus,
     expected: usize,
 ) {
     let count = store.count_by_status(status).await.unwrap();
-    assert_eq!(count, expected);
+    assert_eq!(
+        count, expected,
+        "Incorrect number of activations with {status:?}"
+    );
+}
+
+/// Create a basic default [`Config`]
+pub fn create_config() -> Arc<Config> {
+    Arc::new(Config::default())
+}
+
+/// Create a Config instance that uses a testing topic
+/// and earliest auto_offset_reset. This is intended to be combined
+/// with [`reset_topic`]
+pub fn create_integration_config() -> Arc<Config> {
+    let config = Config {
+        kafka_topic: "taskbroker-test".into(),
+        kafka_auto_offset_reset: "earliest".into(),
+        ..Config::default()
+    };
+
+    Arc::new(config)
+}
+
+/// Create a kafka producer for a given config
+pub fn create_producer(config: Arc<Config>) -> Arc<FutureProducer> {
+    let producer: FutureProducer = config
+        .kafka_producer_config()
+        .create()
+        .expect("Could not create kafka producer");
+
+    Arc::new(producer)
+}
+
+/// Reset a kafka topic by destroying it and recreating it.
+pub async fn reset_topic(config: Arc<Config>) {
+    let admin_client: AdminClient<_> = config
+        .kafka_producer_config()
+        .create()
+        .expect("Could not create admin client");
+
+    let options = AdminOptions::default();
+    admin_client
+        .delete_topics(
+            &[config.kafka_topic.as_ref(), &config.kafka_deadletter_topic],
+            &options,
+        )
+        .await
+        .expect("Could not delete topic");
+    let new_topic = NewTopic::new(&config.kafka_topic, 1, TopicReplication::Fixed(0));
+    let new_dlq_topic = NewTopic::new(
+        &config.kafka_deadletter_topic,
+        1,
+        TopicReplication::Fixed(0),
+    );
+    admin_client
+        .create_topics([&new_topic, &new_dlq_topic], &options)
+        .await
+        .expect("Could not create topic");
+}
+
+/// Consume a number of messages from topic based on [`Config`].
+/// Will wait up to 30s for messages to arrive before panicing.
+pub async fn consume_topic(
+    config: Arc<Config>,
+    topic: &str,
+    num_records: usize,
+) -> Vec<TaskActivation> {
+    let consumer: StreamConsumer = config
+        .kafka_consumer_config()
+        .create()
+        .expect("could not create consumer");
+    consumer.subscribe(&[topic]).expect("could not subscribe");
+
+    let mut stream = consumer.stream();
+    let mut results: Vec<TaskActivation> = vec![];
+    let start = Utc::now();
+    loop {
+        let current = Utc::now();
+        if current.timestamp() - start.timestamp() > 30 {
+            panic!("Timed out waiting for messages from consumer");
+        }
+        if results.len() == num_records {
+            break;
+        }
+        let message_opt = stream.next().await;
+        let message_res = match message_opt {
+            Some(result) => result,
+            None => panic!("No message received"),
+        };
+        let message = match message_res {
+            Ok(msg) => msg,
+            Err(err) => panic!("Message is an error {err:?}"),
+        };
+        let payload = message.payload().expect("Could not fetch message payload");
+        let activation = TaskActivation::decode(payload).unwrap();
+        results.push(activation);
+    }
+
+    results
 }
