519c8020cfa9fa8187132ca8e01ff9ee763fd1da
John Yang <john.yang@sentry.io>
2025-05-20 09:04:25 -0700

    fix ActivationWriterConfig to use correct limit

--- src/kafka/inflight_activation_writer.rs
+++ src/kafka/inflight_activation_writer.rs
@@ -30,7 +30,7 @@ impl ActivationWriterConfig {
     /// Convert from application configuration into InflightActivationWriter config.
     pub fn from_config(config: &Config) -> Self {
         Self {
-            max_buf_len: config.db_insert_batch_size_bytes,
+            max_buf_len: config.db_insert_batch_size_rows,
             max_pending_activations: config.max_pending_count,
             max_delay_activations: config.max_delay_count,
             write_failure_backoff_ms: config.db_write_failure_backoff_ms,
