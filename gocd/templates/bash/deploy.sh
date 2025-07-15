697eaa5a4c918efc63dbc53da2b66a73d38e8696
mchen-sentry <icswon1108@gmail.com>
2025-07-15 11:44:09 -0700

    fix gocd bash error

--- gocd/templates/bash/deploy.sh
+++ gocd/templates/bash/deploy.sh
@@ -2,8 +2,8 @@
 
 eval $(regions-project-env-vars --region="${SENTRY_REGION}")
 
-/devinfra/scripts/get-cluster-credentials &&
-	k8s-deploy \
+/devinfra/scripts/get-cluster-credentials \
+&& k8s-deploy \
 		--label-selector="${LABEL_SELECTOR}" \
 		--image="us-central1-docker.pkg.dev/sentryio/taskbroker/image:${GO_REVISION_TASKBROKER_REPO}" \
 		--type="statefulset" \
