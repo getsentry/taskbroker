a82db4a9702a9bd7a3887cefa87addb98def5849
Tony Le <tony.le@sentry.io>
2025-11-21 12:14:54 -0500

    fix(deploy): GH actions check for correct job name (#514)

--- gocd/templates/bash/check-github-runs.sh
+++ gocd/templates/bash/check-github-runs.sh
@@ -3,5 +3,6 @@
 checks-githubactions-checkruns \
 	"getsentry/taskbroker" \
 	"${GO_REVISION_TASKBROKER_REPO}" \
-	"Build and push production images" \
+	"Build and push production images to single-region registry" \
+	"Build and push production images to multi-region registry" \
 	"Tests (ubuntu)"
