7dae286c71e486bac70816d5a72d04e4c2af4d43
Mark Story <mark@mark-story.com>
2025-11-28 15:04:14 -0500

    Move structs out of separate modules
    Combine small modules together now that import cycles are gone

--- clients/python/src/taskbroker_client/types.py
+++ clients/python/src/taskbroker_client/types.py
@@ -1,6 +1,8 @@
+import dataclasses
 from typing import Callable, Protocol
 
 from arroyo.backends.kafka import KafkaProducer
+from sentry_protos.taskbroker.v1.taskbroker_pb2 import TaskActivation, TaskActivationStatus
 
 
 class AtMostOnceStore(Protocol):
@@ -15,3 +17,23 @@ ProducerFactory = Callable[[str], KafkaProducer]
 A factory interface for resolving topics into a KafkaProducer
 that can produce on the provided topic.
 """
+
+
+@dataclasses.dataclass
+class InflightTaskActivation:
+    """
+    A TaskActivation with Metadata used within workers.
+    """
+    activation: TaskActivation
+    host: str
+    receive_timestamp: float
+
+
+@dataclasses.dataclass
+class ProcessingResult:
+    """Result structure from child processess to parent"""
+
+    task_id: str
+    status: TaskActivationStatus.ValueType
+    host: str
+    receive_timestamp: float
