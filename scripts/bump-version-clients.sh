d454f6f782b0a915d12542e42c5ee636bd61c697
Mark Story <mark@mark-story.com>
2026-02-11 11:46:11 -0500

    fix(client) Fix mistakes in the client release tools
    - Add a name to the workflow.
    - Fix changelogPolicy deprecation
    - Define NEW_VERSION
    
    Refs STREAM-717

--- scripts/bump-version-clients.sh
+++ scripts/bump-version-clients.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$(dirname $SCRIPT_DIR)
 
+OLD_VERSION="${1}"
+NEW_VERSION="${2}"
+
 cd $ROOT_DIR/clients/python
 
 uv version "${NEW_VERSION}"
