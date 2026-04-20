a5e781b55b8f342213dfd104fe67bbe390b16781
Evan Hicks <evanh@users.noreply.github.com>
2026-04-20 16:05:01 -0400

    feat: Separate push Taskworker from pull Taskworker (#593)
    * feat: Separate push Taskworker from pull Taskworker
    
    Moving the push taskbroker to a k8s Deployment means that the worker logic can simplify.
    Particularly the push worker doesn't need a list of hosts, just a single service host. Instead of
    having one Taskworker that supports both modes, create separate classes and clients for each.
    
    This meant moving the multiprocess part of the TaskWorker to a separate class, that manages the
    queues and the various children processes and threads. This class is shared by both the Push worker
    and the original pull worker.
    The push-based taskworker doesn't need all the features of the pull based taskbroker. Create a new
    simpler client that has a single RPC host to connect t
    
    * fixes
    
    * fix
    
    * raise"
    
    * fixes
    
    * fixes
    
    * fix
    
    * fix
    
    * more cleanup
    
    * fixes
    
    * Update clients/python/src/taskbroker_client/worker/client.py
    
    Co-authored-by: Mark Story <mark@mark-story.com>
    
    * Update clients/python/src/taskbroker_client/worker/client.py
    
    Co-authored-by: Mark Story <mark@mark-story.com>
    
    * fixes
    
    * merge
    
    ---------
    
    Co-authored-by: Mark Story <mark@mark-story.com>

--- clients/python/src/taskbroker_client/worker/client.py
+++ clients/python/src/taskbroker_client/worker/client.py
@@ -486,3 +486,106 @@ class TaskbrokerClient:
                 receive_timestamp=time.monotonic(),
             )
         return None
+
+
+class PushTaskbrokerClient:
+    """
+    Taskworker RPC client wrapper
+
+    Push brokers are a deployment so they don't need to be connected to individually. There is one service provided
+    that works for all the brokers.
+    """
+
+    def __init__(
+        self,
+        service: str,
+        application: str,
+        metrics: MetricsBackend,
+        health_check_settings: HealthCheckSettings | None = None,
+        rpc_secret: str | None = None,
+        grpc_config: str | None = None,
+    ) -> None:
+        self._application = application
+        self._service = service
+        self._rpc_secret = rpc_secret
+        self._metrics = metrics
+
+        self._grpc_options: list[tuple[str, Any]] = [
+            ("grpc.max_receive_message_length", MAX_ACTIVATION_SIZE)
+        ]
+        if grpc_config:
+            self._grpc_options.append(("grpc.service_config", grpc_config))
+
+        logger.info(
+            "taskworker.push_client.start",
+            extra={"service": service, "options": self._grpc_options},
+        )
+
+        self._stub = self._connect_to_host(service)
+
+        self._health_check_settings = health_check_settings
+        self._timestamp_since_touch_lock = threading.Lock()
+        self._timestamp_since_touch = 0.0
+
+    def _emit_health_check(self) -> None:
+        if self._health_check_settings is None:
+            return
+
+        with self._timestamp_since_touch_lock:
+            cur_time = time.time()
+            if (
+                cur_time - self._timestamp_since_touch
+                < self._health_check_settings.touch_interval_sec
+            ):
+                return
+
+            self._health_check_settings.file_path.touch()
+            self._metrics.incr(
+                "taskworker.client.health_check.touched",
+            )
+            self._timestamp_since_touch = cur_time
+
+    def _connect_to_host(self, host: str) -> ConsumerServiceStub:
+        logger.info("taskworker.push_client.connect", extra={"host": host})
+        channel = grpc.insecure_channel(host, options=self._grpc_options)
+        secrets = parse_rpc_secret_list(self._rpc_secret)
+        if secrets:
+            channel = grpc.intercept_channel(channel, RequestSignatureInterceptor(secrets))
+        return ConsumerServiceStub(channel)
+
+    def update_task(
+        self,
+        processing_result: ProcessingResult,
+    ) -> None:
+        """
+        Update the status for a given task activation.
+        """
+        self._emit_health_check()
+
+        request = SetTaskStatusRequest(
+            id=processing_result.task_id,
+            status=processing_result.status,
+            fetch_next_task=None,
+        )
+
+        retries = 0
+        exception = None
+        while retries < 3:
+            try:
+                with self._metrics.timer(
+                    "taskworker.update_task.rpc", tags={"service": self._service}
+                ):
+                    self._stub.SetTaskStatus(request)
+                exception = None
+                break
+            except grpc.RpcError as err:
+                exception = err
+                self._metrics.incr(
+                    "taskworker.client.rpc_error",
+                    tags={"method": "SetTaskStatus", "status": err.code().name},
+                )
+            finally:
+                retries += 1
+
+        if exception:
+            raise exception
