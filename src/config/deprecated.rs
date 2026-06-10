84ade5a3e0085e7265bddeffc132a75ca76b5b3b
james-mcnulty <george.berdovskiy@sentry.io>
2026-06-10 15:47:38 -0700

    More Comments

--- src/config/deprecated.rs
+++ src/config/deprecated.rs
@@ -5,12 +5,14 @@ use crate::config::store::DatabaseAdapter;
 macro_rules! map {
     () => {};
 
+    // An optional deprecated value ALWAYS wins
     ($deprecated:expr => some($current:expr), $($rest:tt)*) => {
         $current = $deprecated.take();
 
         $crate::config::deprecated::map!($($rest)*);
     };
 
+    // A plain deprecated value ONLY wins when it's provided
     ($deprecated:expr => $current:expr, $($rest:tt)*) => {
         if let Some(v) = $deprecated.take() {
             $current = v;
@@ -19,10 +21,12 @@ macro_rules! map {
         $crate::config::deprecated::map!($($rest)*);
     };
 
+    // An optional deprecated value ALWAYS wins
     ($deprecated:expr => some($current:expr)) => {
         $current = $deprecated.take();
     };
 
+    // A plain deprecated value ONLY wins when it's provided
     ($deprecated:expr => $current:expr) => {
         if let Some(v) = $deprecated.take() {
             $current = v;
