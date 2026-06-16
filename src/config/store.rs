c26741f61773191fb37e5d91e3b92dcb4db7ff24
james-mcnulty <george.berdovskiy@sentry.io>
2026-06-16 09:08:05 -0700

    Delete Old, Broken Imports

--- src/config/store.rs
+++ src/config/store.rs
@@ -5,8 +5,6 @@ use serde::{Deserialize, Serialize};
 use tracing::warn;
 
 use crate::config::Config;
-use crate::config::deserialize;
-use crate::config::serialize;
 use crate::store::adapters::postgres;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash, Deserialize, Serialize)]
